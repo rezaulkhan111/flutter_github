3aeed138f232214b955761a9daf8b21564a4fbb3
rezaulkhan111 <rezaulkhan111@hotmail.com>
2025-11-20 19:12:48 +0600

    add retrofit

---
+++ lib/data/api_service.dart
@@ -0,0 +1,10 @@
+
+import 'package:retrofit/retrofit.dart';
+
+// @RestApi(baseUrl: "https://jsonplaceholder.typicode.com")
+// abstract class ApiService {
+//   factory ApiService(Dio dio) = ApiService;
+//
+//   @GET("/users/{id}")
+//   Future<User> getUser(@Path("id") int id);
+// }
